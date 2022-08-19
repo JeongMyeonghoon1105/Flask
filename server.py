42166e7ca3ddf7eb52b55cacb3280855a82aeeff
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-08-20 07:32:58 +0900

    Comments

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.static_folder = 'static'
 
+# 현재 표시중인 이미지의 번호
 i = 0
 
 
@@ -16,6 +17,7 @@ def hello():
 
 @app.route('/method')
 def method():
+    # 이미지 파일 리셋
     filePath = 'static/result'
     if os.path.exists(filePath):
         for file in os.scandir(filePath):
@@ -24,6 +26,8 @@ def method():
     if os.path.exists(filePath):
         for file in os.scandir(filePath):
             os.remove(file.path)
+
+    # index.html 실행
     return render_template('/index.html')
 
 
@@ -39,13 +43,14 @@ def upload():
 
 @app.route('/search')
 def search():
-    # AI를 활용해 비슷한 이미지 찾기
+    # AI로 비슷한 이미지 찾기
     # 찾은 이미지들을 static/result 폴더에 저장
     return redirect('/result', code=302)
 
 
 @app.route('/result')
 def result():
+    # 검색 결과 Render
     global i
     imgs = os.listdir('static/result')
 
@@ -62,6 +67,7 @@ def result():
 
 @app.route('/up')
 def up():
+    # 이전 이미지로 이동
     if len(os.listdir('static/result')) != 0:
         global i
         if i != 0:
@@ -73,6 +79,7 @@ def up():
 
 @app.route('/down')
 def down():
+    # 다음 이미지로 이동
     if len(os.listdir('static/result')) != 0:
         global i
         if i != len(os.listdir('static/result'))-1:
